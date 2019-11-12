3a82114c44ea911c23d20a5f233f8b8613c09cb8
Frederico Fávaro Ribeiro <fredericofavaro@gmail.com>
2019-11-12 15:44:36 -0300

    Finalizado

--- basic/list1.py
+++ basic/list1.py
@@ -22,8 +22,11 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-    # +++your code here+++
-    return
+    string_number=0
+    for word in words:
+        if len(word) >= 2 and word[0] == word[-1]:
+            string_number += 1
+    return string_number
 
 
 # B. front_x
@@ -34,8 +37,17 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    # +++your code here+++
-    return
+    listaX=[]
+    lista=[]
+    words.sort()
+    for word in words:
+        if word[0] == "x":
+            listaX.append(word)
+        else:
+            lista.append(word)
+    return listaX+lista
+
+
 
 
 # C. sort_last
@@ -45,7 +57,8 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    # +++your code here+++
+    tuples.sort(key=)
+    
     return
 
 
